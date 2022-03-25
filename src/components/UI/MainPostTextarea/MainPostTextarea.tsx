a78227f61dfd6347e7f6970eff355dfa52e0d8d4
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-03-25 19:47:09 +0100

    Finish progress check functional

--- src/components/UI/MainPostTextarea/MainPostTextarea.tsx
+++ src/components/UI/MainPostTextarea/MainPostTextarea.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import s from "./MainPostTextarea.module.css";
 import { Field } from "react-final-form";
 import SimpleForm from "../../../common/SimpleForm";
@@ -6,8 +6,26 @@ import TextareaAutosize from "react-textarea-autosize";
 import { CircularProgress } from "@mui/material";
 
 const MainPostTextarea = () => {
+  const maxSymbols = 300;
+  const [length, setLength] = useState(0);
+  const [left, setLeft] = useState(maxSymbols);
   const onTextareaSend = (data: object) => {
     console.log(data);
+  };
+
+  const getColor = (length: number) => {
+    if (length > maxSymbols) {
+      return "secondary";
+    } else if (length >= 280) {
+      return "warning";
+    }
+    return "primary";
+  };
+
+  const onTextAreaChange = (e: any, onChange: any) => {
+    onChange(e);
+    setLength(e.target.value.length);
+    setLeft(maxSymbols - e.target.value.length);
   };
 
   return (
@@ -23,7 +41,7 @@ const MainPostTextarea = () => {
                 autoFocus
                 name={props.input.name}
                 value={props.input.value}
-                onChange={props.input.onChange}
+                onChange={(e) => onTextAreaChange(e, props.input.onChange)}
                 placeholder="What's Happening?"
                 minRows={1}
                 maxRows={20}
@@ -31,12 +49,25 @@ const MainPostTextarea = () => {
             )}
           </Field>
           <div className={s.form_buttons}>
-            <div className="group">
-              <button type="button" className={`${s.btn} ${s.btn_media}`}>Media</button>
-              <button type="button" className={`${s.btn} ${s.btn_emoji}`}>Emoji</button>
+            <div className={s.btn_group}>
+              <button type="button" className={`${s.btn} ${s.btn_media}`}>
+                Media
+              </button>
+              <button type="button" className={`${s.btn} ${s.btn_emoji}`}>
+                Emoji
+              </button>
             </div>
-            <CircularProgress size={22} color="primary" thickness={4.5} variant="determinate" value={75} />
-            <button type="submit">Tweet</button>
+            <div className="progressBar">
+              <span className={left < 0 ? 'warning' : ''}>{left}</span>
+              <CircularProgress
+                size={22}
+                color={getColor(length)}
+                thickness={4.5}
+                variant="determinate"
+                value={length <= 300 ? Math.ceil(length / 3) : 100}
+              />
+            </div>
+            <button type="submit" disabled={length > maxSymbols || length < 1}>Tweet</button>
           </div>
         </SimpleForm>
       </div>
