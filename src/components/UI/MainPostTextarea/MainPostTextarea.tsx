55f9f2fbf41e9fbf39ee2c01699f079cf1304898
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-03-25 19:04:19 +0100

    Add custom progressbar

--- src/components/UI/MainPostTextarea/MainPostTextarea.tsx
+++ src/components/UI/MainPostTextarea/MainPostTextarea.tsx
@@ -3,6 +3,7 @@ import s from "./MainPostTextarea.module.css";
 import { Field } from "react-final-form";
 import SimpleForm from "../../../common/SimpleForm";
 import TextareaAutosize from "react-textarea-autosize";
+import { CircularProgress } from "@mui/material";
 
 const MainPostTextarea = () => {
   const onTextareaSend = (data: object) => {
@@ -19,6 +20,7 @@ const MainPostTextarea = () => {
           <Field name="post_content">
             {(props) => (
               <TextareaAutosize
+                autoFocus
                 name={props.input.name}
                 value={props.input.value}
                 onChange={props.input.onChange}
@@ -29,8 +31,11 @@ const MainPostTextarea = () => {
             )}
           </Field>
           <div className={s.form_buttons}>
-            <button type="button" className={`${s.btn} ${s.btn_img}`}></button>
-            <button type="button" className={`${s.btn} ${s.btn_emoji}`}></button>
+            <div className="group">
+              <button type="button" className={`${s.btn} ${s.btn_media}`}>Media</button>
+              <button type="button" className={`${s.btn} ${s.btn_emoji}`}>Emoji</button>
+            </div>
+            <CircularProgress size={22} color="primary" thickness={4.5} variant="determinate" value={75} />
             <button type="submit">Tweet</button>
           </div>
         </SimpleForm>
