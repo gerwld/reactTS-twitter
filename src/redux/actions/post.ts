351bdd16129143df19d921f6f08b4c2d25d052a9
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-03-26 12:58:57 +0100

    Add actions interface

--- src/redux/actions/post.ts
+++ src/redux/actions/post.ts
@@ -1,15 +1,21 @@
+import { Action } from "redux"
 
 export enum PostActionsTypes {
   ON_POST_TYPE = "twitter-ts/post/ON_POST_TYPE",
   GET_LAST_POSTS = "twitter-ts/post/GET_LAST_POSTS"
 }
 
-export const onPostType = (value: string) => ({
+export interface PostActionInterface extends Action {
+  type: PostActionsTypes,
+  payload: object | string
+}
+
+export const onPostType = (value: string): PostActionInterface => ({
   type: PostActionsTypes.ON_POST_TYPE,
   payload: value
 })
 
-export const getAllPosts = (obj: object) => ({
+export const getAllPosts = (obj: object): PostActionInterface => ({
   type: PostActionsTypes.GET_LAST_POSTS,
   payload: obj
 })
